d36949430756e2ee2ee881aeea00763a65dfcb73
ivmateius <viorelmateius@gmail.com>
2025-01-20 06:15:16 +0200

    first commit

---
+++ src/main/java/vio/ionut/App.java
@@ -0,0 +1,13 @@
+package vio.ionut;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
